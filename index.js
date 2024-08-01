e5e00d8825ace3a13527e1b42fd734b1a637fb06
akglitch <mcaninakwa@gmail.com>
2024-07-31 20:40:40 -0700

    added port to .env

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use('/api', governmentAppointeeRoutes);
 app.use('/api', subcommitteesRoutes);
 app.use('/api', memberRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
