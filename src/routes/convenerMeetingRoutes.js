a81628143d24bf5afffab521e482078c7973b8e2
akglitch <mcaninakwa@gmail.com>
2024-11-02 23:46:38 -0200

    minor tweaks

--- src/routes/convenerMeetingRoutes.js
+++ src/routes/convenerMeetingRoutes.js
@@ -2,14 +2,14 @@
 
 const express = require('express');
 const router = express.Router();
-const { recordConvenerMeetingAttendance,getTotalConveners, deleteAllAttendanceRecords, fetchConvenerAttendanceReport,fetchConveners } = require('../controllers/convenerMeetingController');
+const { recordConvenerMeetingAttendance,getTotalConveners,getTotalAttendance, deleteAllAttendanceRecords, fetchConvenerAttendanceReport,fetchConveners } = require('../controllers/convenerMeetingController');
 
 router.post('/convener-meeting/attendance', recordConvenerMeetingAttendance);
 router.get('/conveners', fetchConveners);
 router.get('/execoreport', fetchConvenerAttendanceReport);
 router.delete('/attendance/deleteAll', deleteAllAttendanceRecords);
 router.get('/totalconveners', getTotalConveners);
-router.get('/totalconvenerattendance', getTotalConveners);
+router.get('/totalconvenerattendance', getTotalAttendance);
 
 
 
